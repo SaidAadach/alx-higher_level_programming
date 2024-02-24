f7bd2f020621edb56bace7f22d3f3dc74ea0d8b8
jkgjfjmf <klhfasklfh@gmail.com>
2024-02-24 22:07:42 +0100

    0x02-python-import_modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    from sys import argv
-    argc = len(argv)
-    result = 0
-    for i in range(1, argc):
-        result += int(argv[i])
-    print(f'{result:d}')
+    """Print the addition of all arguments."""
+    import sys
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def add(a, b):
     """My addition function
 
@@ -7,18 +8,45 @@ def add(a, b):
         b: second integer
 
     Returns:
-        The return value. a+b
+        The return value. a + b
     """
     return (a + b)
 
 
 def sub(a, b):
-    return(a - b)
+    """My subtraction function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
 
 
 def mul(a, b):
+    """My multiplication function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a * b
+    """
     return (a * b)
 
 
 def div(a, b):
+    """My division function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a / b
+    """
     return int(a / b)
